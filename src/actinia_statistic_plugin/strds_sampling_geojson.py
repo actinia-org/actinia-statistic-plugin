44702f49dafbf1441e527b28d4d77cc93dedae25
Carmen <carmen.tawalika@gmail.com>
2021-06-02 17:08:50 +0200

    move actinia_core imports

--- src/actinia_statistic_plugin/strds_sampling_geojson.py
+++ src/actinia_statistic_plugin/strds_sampling_geojson.py
@@ -9,10 +9,10 @@ from flask import jsonify, make_response
 from flask.json import dumps as json_dumps
 from copy import deepcopy
 from flask_restful_swagger_2 import swagger
-from actinia_core.resources.common.response_models import ProcessingResponseModel, ProcessingErrorResponseModel
-from actinia_core.resources.ephemeral_processing import EphemeralProcessing
-from actinia_core.resources.resource_base import ResourceBase
-from actinia_core.resources.common.redis_interface import enqueue_job
+from actinia_core.models.response_models import ProcessingResponseModel, ProcessingErrorResponseModel
+from actinia_core.rest.ephemeral_processing import EphemeralProcessing
+from actinia_core.rest.resource_base import ResourceBase
+from actinia_core.core.common.redis_interface import enqueue_job
 
 __license__ = "GPLv3"
 __author__ = "Sören Gebbert"
