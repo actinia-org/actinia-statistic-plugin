1547cd370f68714d33dcf8fe6e03844ee9a0a382
anikaweinmann <aweinmann@mundialis.de>
2022-07-29 10:49:55 +0200

    fix test_strds_area_stats.py

--- tests/test_strds_area_stats.py
+++ tests/test_strds_area_stats.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import unittest
-# import time
-# from flask.json import loads as json_load
-# from flask.json import dumps as json_dump
-#
-# try:
-#     from .test_resource_base import ActiniaResourceTestCaseBase, URL_PREFIX
-# except Exception:
-#     from test_resource_base import ActiniaResourceTestCaseBase, URL_PREFIX
+import time
+from flask.json import loads as json_load
+from flask.json import dumps as json_dump
+
+from pprint import pprint
+
+try:
+    from .test_resource_base import ActiniaResourceTestCaseBase, URL_PREFIX
+except Exception:
+    from test_resource_base import ActiniaResourceTestCaseBase, URL_PREFIX
 
 __license__ = "GPLv3"
 __author__ = "Sören Gebbert"
@@ -15,7 +17,6 @@ __copyright__ = "Copyright 2016, Sören Gebbert"
 __maintainer__ = "Soeren Gebbert"
 __email__ = "soerengebbert@googlemail.com"
 
-# TODO use modis data
 LOCATION = "nc_spm_08"
 MAPSET = "modis_lst"
 STRDS = "LST_Day_monthly"
@@ -24,7 +25,7 @@ JSON = {
     "type": "FeatureCollection",
     "crs": {
         "type": "name",
-        "properties": {"name": "urn:ogc:def:crs:EPSG::3358"},
+        "properties": {"name": "urn:x-ogc:def:crs:EPSG:3358"}
     },
     "features": [
         {
@@ -34,24 +35,29 @@ JSON = {
                 "type": "Polygon",
                 "coordinates": [
                     [
-                        [349852.196963928756304, 252507.816825738176703],
-                        [349852.196963928756304, 244101.739720351994038],
-                        [361635.941179184708744, 244101.739720351994038],
-                        [361635.941179184708744, 252507.816825738176703],
-                        [349852.196963928756304, 252507.816825738176703],
+                        [330000.0, 65000.0],
+                        [337000.0, 65000.0],
+                        [337000.0, 647000.0],
+                        [330000.0, 647000.0],
+                        [330000.0, 65000.0],
                     ]
                 ],
             },
         }
     ],
 }
+TIMESTAMP = "2016-01-01T00:00:00"
+BASE_URL = f"{URL_PREFIX}/locations/{LOCATION}/mapsets/{MAPSET}/strds/" \
+    f"{STRDS}/timestamp/{TIMESTAMP}"
+ASYNC_URL = f"{BASE_URL}/area_stats_async"
+SYNC_URL = f"{BASE_URL}/area_stats_sync"
 
-"""
+
 class STRDSAreaStatsTestCase(ActiniaResourceTestCaseBase):
     def test_async_raster_area_stats_json(self):
+
         rv = self.server.post(
-            f"{URL_PREFIX}/locations/{LOCATION}/mapsets/{MAPSET}/strds/"
-            f"{STRDS}/timestamp/2016-01-01T00:00:00/area_stats_async",
+            ASYNC_URL,
             headers=self.admin_auth_header,
             data=json_dump(JSON),
             content_type="application/json",
@@ -70,12 +76,12 @@ class STRDSAreaStatsTestCase(ActiniaResourceTestCaseBase):
             rv, headers=self.admin_auth_header
         )
 
-        self.assertEqual(len(data["process_results"]), 29)
+        self.assertEqual(len(data["process_results"]), 93)
 
     def test_sync_raster_area_stats_1(self):
+
         rv = self.server.post(
-            f"{URL_PREFIX}/locations/{LOCATION}/mapsets/{MAPSET}/strds/"
-            f"{STRDS}/timestamp/2016-01-01T00:00:00/area_stats_sync",
+            SYNC_URL,
             headers=self.admin_auth_header,
             data=json_dump(JSON),
             content_type="application/json",
@@ -91,16 +97,12 @@ class STRDSAreaStatsTestCase(ActiniaResourceTestCaseBase):
         )
 
         value_list = json_load(rv.data)["process_results"]
-        self.assertEqual(len(value_list), 29)
+        self.assertEqual(len(value_list), 93)
 
     def test_sync_raster_area_stats_2(self):
+
         rv = self.server.post(
-            f"{URL_PREFIX}/locations/{LOCATION}/mapsets/{MAPSET}/strds/"
-            f"{STRDS}/timestamp/2016-01-01T00:00:00/area_stats_sync",
-            # URL_PREFIX + '/locations/ECAD/mapsets/PERMANENT/strds/'
-            #                       'temperature_mean_1950_2013_yearly_celsius/'
-            #                       'timestamp/2001-01-01T00:00:00/'
-            #                       'area_stats_sync',
+            SYNC_URL,
             headers=self.admin_auth_header,
             data=json_dump(JSON),
             content_type="application/json",
@@ -116,19 +118,19 @@ class STRDSAreaStatsTestCase(ActiniaResourceTestCaseBase):
         )
 
         value_list = json_load(rv.data)["process_results"]
-        self.assertEqual(len(value_list), 29)
+        self.assertEqual(len(value_list), 93)
 
     def test_sync_raster_area_stats_error_no_map_found(self):
+
+        newer_timestamp = "2021-01-01T00:00:00"
         rv = self.server.post(
-            URL_PREFIX + "/locations/ECAD/mapsets/PERMANENT/strds/"
-            "temperature_mean_1950_2013_yearly_celsius/"
-            "timestamp/2021-01-01T00:00:00/"
-            "area_stats_sync",
+            SYNC_URL.replace(TIMESTAMP, newer_timestamp),
             headers=self.admin_auth_header,
             data=json_dump(JSON),
             content_type="application/json",
         )
 
+        pprint(json_load(rv.data))
         self.assertEqual(
             rv.status_code,
             400,
@@ -139,11 +141,9 @@ class STRDSAreaStatsTestCase(ActiniaResourceTestCaseBase):
         )
 
     def test_sync_raster_area_stats_wrong_content_type(self):
+
         rv = self.server.post(
-            URL_PREFIX + "/locations/ECAD/mapsets/PERMANENT/strds/"
-            "temperature_mean_1950_2013_yearly_celsius/"
-            "timestamp/2001-01-01T00:00:00/"
-            "area_stats_sync",
+            SYNC_URL,
             headers=self.admin_auth_header,
             data=" This is no data",
             content_type="application/XML",
@@ -160,10 +160,7 @@ class STRDSAreaStatsTestCase(ActiniaResourceTestCaseBase):
 
     def test_sync_raster_area_stats_error_nodata(self):
         rv = self.server.post(
-            URL_PREFIX + "/locations/ECAD/mapsets/PERMANENT/strds/"
-            "temperature_mean_1950_2013_yearly_celsius/"
-            "timestamp/2001-01-01T00:00:00/"
-            "area_stats_sync",
+            SYNC_URL,
             headers=self.admin_auth_header,
             data=json_dump({}),
             content_type="application/json",
@@ -180,15 +177,13 @@ class STRDSAreaStatsTestCase(ActiniaResourceTestCaseBase):
 
     def test_sync_raster_area_stats_error(self):
         rv = self.server.post(
-            URL_PREFIX + "/locations/ECAD/mapsets/PERMANENT/strds/"
-            "temperature_mean_1950_2013_yearly_celsius/"
-            "timestamp/2001-01-01T00:00:00/"
-            "area_stats_sync",
+            SYNC_URL,
             headers=self.admin_auth_header,
             data=None,
             content_type="application/json",
         )
 
+        pprint(json_load(rv.data))
         self.assertEqual(
             rv.status_code,
             400,
@@ -199,16 +194,16 @@ class STRDSAreaStatsTestCase(ActiniaResourceTestCaseBase):
         )
 
     def test_sync_raster_area_stats_error_wrong_timestamp(self):
+
+        wrong_timestamp = "2016-01-01T00.00.00"
         rv = self.server.post(
-            URL_PREFIX + "/locations/ECAD/mapsets/PERMANENT/strds/"
-            "temperature_mean_1950_2013_yearly_celsius/"
-            "timestamp/2001-01-01T00.00.00/"
-            "area_stats_sync",
+            SYNC_URL.replace(TIMESTAMP, wrong_timestamp),
             headers=self.admin_auth_header,
             data=json_dump(JSON),
             content_type="application/json",
         )
 
+        pprint(json_load(rv.data))
         self.assertEqual(
             rv.status_code,
             400,
@@ -217,7 +212,6 @@ class STRDSAreaStatsTestCase(ActiniaResourceTestCaseBase):
         self.assertEqual(
             rv.mimetype, "application/json", "Wrong mimetype %s" % rv.mimetype
         )
-"""
 
 
 if __name__ == "__main__":
